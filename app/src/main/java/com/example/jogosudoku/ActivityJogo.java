675704c2cac2ece95e505bf792a5952c03082d05
MarcoFidalgo <m_s_f_@hotmail.com>
2019-11-13 23:07:28 +0000

    Começo Projeto
    Janela Principal
     - onClick dos botoes, leva no intentextra o nº do jogo
    Janela 1Jogador
     - alguma parte estética

---
+++ app/src/main/java/com/example/jogosudoku/ActivityJogo.java
@@ -0,0 +1,14 @@
+package com.example.jogosudoku;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityJogo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jogo);
+    }
+}
--- app/src/main/java/com/example/jogosudoku/MainActivity.java
+++ app/src/main/java/com/example/jogosudoku/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.jogosudoku;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Tratamento Botoes Menu Inicial
+    public void trataBotaoM1(View view) {
+        Intent intent = new Intent(MainActivity.this, ActivityJogo.class);
+        intent.putExtra("gameMode", 1);
+        startActivity(intent);
+    }
+
+    public void trataBotaoM2(View view) {
+        Intent intent = new Intent(MainActivity.this, ActivityJogo.class);
+        intent.putExtra("gameMode", 2);
+        startActivity(intent);
+    }
+
+    public void trataBotaoM3(View view) {
+        Intent intent = new Intent(MainActivity.this, ActivityJogo.class);
+        intent.putExtra("gameMode", 3);
+        startActivity(intent);
     }
 }
